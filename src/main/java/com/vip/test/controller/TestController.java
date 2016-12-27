105d1d4a466d876c51343823862dd0c2a466e37d
jack19910921 <870313180@qq.com>
2016-12-27 17:11:10 +0800

    delete

--- src/main/java/com/vip/test/controller/TestController.java
+++ src/main/java/com/vip/test/controller/TestController.java
@@ -1,13 +1,8 @@
 package com.vip.test.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.google.common.collect.ImmutableMap;
-import com.vip.test.entity.BusDataDict;
 import com.vip.test.service.TestService;
 import lombok.extern.java.Log;
-import org.kiwi.http.support.HttpCallback;
 import org.kiwi.http.support.HttpTemplate;
-import org.kiwi.http.support.exception.HttpException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,25 +23,11 @@ public class TestController {
 
     @RequestMapping(value = "/xx", method = RequestMethod.POST)
     public void xx() {
-        BusDataDict busDataDict = new BusDataDict();//testService.findValueByKey("CERTIFY.OSP.TIMEOUT");
-        busDataDict.setCfgKey("CERTIFY.OSP.TIMEOUT");
-        busDataDict.setCfgValue("45000");
-        testService.updateByPrimaryKeySelective(busDataDict);
-        log.info(JSON.toJSONString(busDataDict));
+
     }
 
     @RequestMapping(value = "/yy", method = RequestMethod.POST)
     public void yy() {
-        try {
-            String result = httpTemplate.doPost("http://xg-open-account.api.vip.com/account/search/queryVipUserNameByIdNo", ImmutableMap.of("idNo", "610122198710093111"), new HttpCallback<String>() {
-                @Override
-                public String doParseResult(String result) throws HttpException {
-                    return result;
-                }
-            });
-            log.info("Result:" + JSON.toJSONString(result));
-        } catch (Exception e) {
-            log.info(e.getMessage());
-        }
+
     }
 }
